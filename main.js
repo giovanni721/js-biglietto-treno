7c338c7a4f4b01276e207002ec4942033d6088a6
giovanni <giovanni.giogio98@gmail.com>
2019-11-14 15:47:34 +0100

    Fine esercizio

---
+++ main.js
@@ -0,0 +1,14 @@
+var km = prompt('Inserire i km da percorrere');
+var eta = prompt('Inserire la tua età');
+var initresult = km * 0.21;
+if (eta > 65) {
+    var anziani = 40 * initresult / 100;
+    document.getElementById('risultato').innerHTML = anziani;
+} else if(eta < 18) {
+    var minorenni = 20 * initresult / 100;
+    document.getElementById('risultato').innerHTML = minorenni;
+} else {
+    document.getElementById('risultato').innerHTML = initresult;
+}
+
+document.getElementById('sentence').setAttribute('class', 'visible');
